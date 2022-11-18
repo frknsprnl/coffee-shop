16f9d713b6e2a8e300b15969f99bdb78e930b361
Furkan <frknsprnl@hotmail.com>
2022-11-18 23:04:01 +0300

    Add: Dynamic Title

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -2,13 +2,29 @@ import React from "react";
 import textLogo from "../../assets/coffee-shop-text-logo.png";
 import logo from "../../assets/coffee-shop-logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBasketShopping, faRightToBracket, faUser } from "@fortawesome/free-solid-svg-icons";
-import './style.css'
-import { Link } from "react-router-dom";
+import {
+  faBasketShopping,
+  faRightToBracket,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import "./style.css";
+import { Link, useLocation } from "react-router-dom";
+import Helmet from "react-helmet";
 
 function Header() {
+  const location = useLocation();
   return (
     <div className="flex px-2 md:px-4 w-full items-center justify-between sticky top-0 z-20 bg-black">
+      <Helmet>
+        <title>
+          {location.pathname === "/"
+            ? "The Coffee Shop"
+            : `${
+                location.pathname.split("/").pop().charAt(0).toLocaleUpperCase() +
+                location.pathname.split("/").pop().slice(1)
+              } | The Coffee Shop`}
+        </title>
+      </Helmet>
       <Link to="/">
         <img
           src={textLogo}
