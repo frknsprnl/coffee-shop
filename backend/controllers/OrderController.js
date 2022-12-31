e9f2c88892bfdfe2d5510adfa29383f03a38b101
Furkan <frknsprnl@hotmail.com>
2022-12-31 23:56:12 +0300

    BE Add: get order

--- backend/controllers/OrderController.js
+++ backend/controllers/OrderController.js
@@ -26,11 +26,37 @@ exports.setOrder = async (req, res) => {
   }
 };
 
+exports.getOrder = async (req, res) => {
+  const { user_id } = req.user;
+  const order_id = req.params.id;
+  const orders = await Order.findOne({
+    user: user_id,
+  }).populate("orders.products.product");
+
+  let orderIndex = -1;
+
+  if (orders) {
+    orderIndex = orders.orders.findIndex(
+      (item) => item._id.toString() === order_id.toString()
+    );
+    if (orderIndex > -1) {
+      res
+        .status(200)
+        .json({ status: "success", orders: orders.orders[orderIndex] });
+    } else {
+      res.status(400).json({ status: "fail", message: "Sipariş bulunamadı." });
+    }
+  } else {
+    res.status(400).json({ status: "fail", message: "Sipariş bulunamadı." });
+  }
+};
+
 exports.getOrders = async (req, res) => {
   const { user_id } = req.user;
-  const orders = await Order.find({ user: user_id })
-    .populate("orders.products.product")
-    
+  const orders = await Order.find({ user: user_id }).populate(
+    "orders.products.product"
+  );
+
   if (orders) {
     res.status(200).json({ status: "success", orders });
   } else {
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.route("/setorder").post(auth, orderController.setOrder);
 router.route("/getorders").get(auth, orderController.getOrders);
+router.route("/getorder/:id").get(auth, orderController.getOrder);
 
 module.exports = router;
