6ae0b222a1786a413261883e1e04d0b1c4ffed9e
Furkan <frknsprnl@hotmail.com>
2023-01-12 23:47:04 +0300

    FE Fix: user nested routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -21,12 +21,18 @@ import Password from "./pages/User/components/Edit/Password";
 import Email from "./pages/User/components/Edit/Email";
 import Address from "./pages/User/components/Edit/Address";
 import Article from "./pages/Article";
+import UserMenu from "./pages/User/components/UserMenu/UserMenu";
 import Edit from "./pages/User/components/Edit/Edit";
 import Error from "./pages/Error";
 import PrivateRoute from "./routes/PrivateRoutes";
 import { RecoilRoot } from "recoil";
 import LoggedInPrivateRoute from "./routes/LoggedInPrivateRoutes";
+import AdminRoute from "./routes/AdminRoutes";
+import AdminPanel from "./pages/AdminPanel";
 import OrderSingle from "./pages/User/components/Order/OrderSingle";
+import AdminUsers from "./pages/AdminPanel/pages/AdminUsers";
+import AdminBlog from "./pages/AdminPanel/pages/AdminBlog";
+import AdminShop from "./pages/AdminPanel/pages/AdminShop";
 
 const router = createBrowserRouter([
   {
@@ -72,27 +78,36 @@ const router = createBrowserRouter([
     element: <PrivateRoute />,
     children: [
       {
-        path: "/user",
+        path: "user",
         element: <User />,
         children: [
+          { path: "", element: <UserMenu /> },
           { path: "profile", element: <Profile /> },
           { path: "orders", element: <Order /> },
           { path: "orders/:orderId", element: <OrderSingle /> },
-          {
-            path: "edit",
-            element: <Edit />,
-            children: [
-              { path: "password", element: <Password /> },
-              { path: "email", element: <Email /> },
-              { path: "address", element: <Address /> },
-            ],
-          },
+          { path: "edit", element: <Edit /> },
+          { path: "edit/password", element: <Password /> },
+          { path: "edit/email", element: <Email /> },
+          { path: "edit/address", element: <Address /> },
         ],
       },
       {
         path: "/basket",
         element: <Basket />,
       },
+    ],
+  },
+  {
+    path: "/",
+    element: <AdminRoute />,
+    children: [
+      {
+        path: "admin",
+        element: <AdminPanel />,
+      },
+      { path: "admin/users", element: <AdminUsers /> },
+      { path: "admin/shop", element: <AdminShop /> },
+      { path: "admin/blog", element: <AdminBlog /> },
     ],
   },
   {
--- frontend/src/pages/User/index.jsx
+++ frontend/src/pages/User/index.jsx
@@ -1,18 +1,10 @@
 import { useEffect } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Outlet, useLocation, useParams } from "react-router-dom";
 import MainLayout from "../../layouts/MainLayout";
-import Profile from "./components/Profile/Profile";
-import UserMenu from "./components/UserMenu/UserMenu";
-import Order from "./components/Order/Order";
-import Edit from "./components/Edit/Edit";
-import Password from "./components/Edit/Password";
-import Email from "./components/Edit/Email";
-import Address from "./components/Edit/Address";
 import { useUserState } from "../../Recoil/User/userState";
 import { useLoginState } from "../../Recoil/User/useLoginState";
 import { useToastState } from "../../Recoil/Error/useToastState";
 import axios from "axios";
-import OrderSingle from "./components/Order/OrderSingle";
 import { useLoadingState } from "../../Recoil/Loading/useLoadingState";
 
 function User() {
@@ -34,9 +26,14 @@ function User() {
         })
         .then((resp) => {
           setUser(resp.data.user);
+          if (resp.data.user.role === "admin") {
+            localStorage.setItem("isAdmin", true);
+          } else {
+            localStorage.removeItem("isAdmin");
+          }
         })
         .catch((err) => {
-          localStorage.removeItem("access-token");
+          localStorage.clear();
           setIsLoggedIn(false);
           setToastMsg({
             isError: false,
@@ -55,23 +52,16 @@ function User() {
           {location.pathname === "/user/profile" && "Profilim"}
           {location.pathname === "/user/orders" && "Siparişlerim"}
           {location.pathname === `/user/orders/${orderId}` && "Siparişim"}
-          {location.pathname === "/user/edit" && "Değiştir"}
-          {location.pathname === "/user/edit/password" && "Şifre Değiştir"}
-          {location.pathname === "/user/edit/email" && "Email Değiştir"}
-          {location.pathname === "/user/edit/address" && "Adres Değiştir"}
+          {location.pathname === "/user/edit" && "Düzenle"}
+          {location.pathname === "/user/edit/password" && "Şifre Düzenle"}
+          {location.pathname === "/user/edit/email" && "Email Düzenle"}
+          {location.pathname === "/user/edit/address" && "Adres Düzenle"}
         </h1>
         <h1 className="text-sm text-white pb-8">
           Hoş geldin <span className="text-[#cda154]">{user.name}</span>!
         </h1>
         <div className="w-full md:w-3/4 lg:w-1/2 flex h-96 border-[1.6px] rounded-xl">
-          {location.pathname === "/user" && <UserMenu />}
-          {location.pathname === "/user/profile" && <Profile />}
-          {location.pathname === "/user/orders" && <Order />}
-          {location.pathname === `/user/orders/${orderId}` && <OrderSingle />}
-          {location.pathname === "/user/edit" && <Edit />}
-          {location.pathname === "/user/edit/password" && <Password />}
-          {location.pathname === "/user/edit/email" && <Email />}
-          {location.pathname === "/user/edit/address" && <Address />}
+          <Outlet />
         </div>
       </div>
     </MainLayout>
